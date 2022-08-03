e329af57a5ee08f9804507345da3c2daf50d9fc7
reginamerino43 <reginamerino43@gmail.com>
2022-08-02 22:09:28 -0500

    1tarea

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let decimal = 0
+  var rev = num.split('').reverse().join('')
 
+  for (let i = 0; i > rev.length; i++) {
+    decimal += rev[i] * 2 ** i
+  }
+  return decimal
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let res = ''
+  while (num > 0){
+    res += num % 2
+    num = Math.floor(num/2)
+  }
+  var binario = res.split('').reverse().join('')
+  return binario
 }
 
 
